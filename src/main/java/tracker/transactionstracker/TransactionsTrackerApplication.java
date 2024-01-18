3aba66c30cd0add686d226f9aa5c6572352360b8
Majchrowski <rafal03m@gmail.com>
2024-01-18 19:33:08 +0100

    Initial commit

---
+++ src/main/java/tracker/transactionstracker/TransactionsTrackerApplication.java
@@ -0,0 +1,13 @@
+package tracker.transactionstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionsTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tracker/transactionstracker/TransactionsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package tracker.transactionstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
